ff4ba26afee816bc1d766f8a3b5843bb5f577734
hyejinme <kimhyejin703@daum.net>
2022-12-16 13:30:49 +0900

    Initial commit

---
+++ src/main/java/com/hj/todoapppreparation/TodoappPreparationApplication.java
@@ -0,0 +1,13 @@
+package com.hj.todoapppreparation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappPreparationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappPreparationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hj/todoapppreparation/TodoappPreparationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hj.todoapppreparation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappPreparationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
